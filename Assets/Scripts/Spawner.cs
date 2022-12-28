6a11c6f6f889f810a45ccfeefa736caaccc8f365
GreatWalker <malekylar@mail.ru>
2022-12-28 13:13:25 +0300

    3
    3

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class Spawner : ObjectPool
+public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _enemyPrefab;
+    [SerializeField] private GameObject _container;
     [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private Enemy[] _enemyPrefab;
     [SerializeField] private float _secondsBetweenSpawn;
+    [SerializeField] private int _capacity;
 
-    private float _elapsedTime = 0;
+    private List<Enemy> _pool = new List<Enemy>();
 
     private void Start()
     {
@@ -16,9 +19,9 @@ public class Spawner : ObjectPool
         StartCoroutine(SpawnEnemy());
     }
 
-    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    private void SetEnemy(Enemy enemy, Vector3 spawnPoint)
     {
-        enemy.SetActive(true);
+        enemy.gameObject.SetActive(true);
         enemy.transform.position = spawnPoint;
     }
 
@@ -26,12 +29,40 @@ public class Spawner : ObjectPool
     {
         while(Time.timeScale != 0)
         {
-            if (TryGetObject(out GameObject enemy))
+            if (TryGetObject(out Enemy enemy))
             {
                 int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
                 SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
                 yield return new WaitForSecondsRealtime(_secondsBetweenSpawn);
             }
         }
+    }
+
+    private void Initialize(Enemy prefab)
+    {
+        for (int i = 0; i < _capacity; i++)
+        {
+            Enemy spawned = Instantiate(prefab, _container.transform);
+            spawned.gameObject.SetActive(false);
+            _pool.Add(spawned);
+        }
+    }
+
+    private void Initialize(Enemy[] prefab)
+    {
+        for (int i = 0; i < _capacity; i++)
+        {
+            int prefabIndex = Random.Range(0, prefab.Length);
+            var spawned = Instantiate(prefab[prefabIndex], _container.transform);
+            spawned.gameObject.SetActive(false);
+            _pool.Add(spawned);
+        }
+    }
+
+    private bool TryGetObject(out Enemy result)
+    {
+        result = _pool.FirstOrDefault(p => p.gameObject.activeSelf == false);
+
+        return result != null;
     }
 }
